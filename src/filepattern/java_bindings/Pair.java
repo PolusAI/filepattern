e9a52ea8120ebb068753ee4ab1c55527647a100c
JesseMckinzie <jessemckinzie145@gmail.com>
2023-04-05 10:35:30 -0600

    Add java api

---
+++ src/filepattern/java_bindings/Pair.java
@@ -0,0 +1,23 @@
+package filepattern2.java_bindings;
+
+public class Pair<T, S> {
+    public T first;
+    public S second;
+    
+    public T get0() {
+        return this.first;
+    }
+
+    public  S get1() {
+        return this.second;
+    }
+
+    public void set0(T value) {
+        this.first = value;
+    }
+
+    public void set1(S value) {
+        this.second = value;
+    }
+    
+}
