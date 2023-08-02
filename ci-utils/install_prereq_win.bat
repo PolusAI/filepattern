3455177625bc1af872296d6354dea43a612d62c7
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2023-08-02 10:47:28 -0600

    Update pybind build

---
+++ ci-utils/install_prereq_win.bat
@@ -0,0 +1,16 @@
+mkdir local_install
+mkdir local_install\include
+
+git clone https://github.com/pybind/pybind11.git
+pushd pybind11
+mkdir build_man
+pushd build_man
+cmake -DCMAKE_INSTALL_PREFIX=../../local_install/  -DPYBIND11_TEST=OFF ..
+cmake --build . --config Release --target install  
+popd
+popd
+
+
+if errorlevel 1 exit 1
+
+if "%ON_GITHUB%"=="TRUE" xcopy /E /I /y local_install\bin %TEMP%\nyxus\bin
