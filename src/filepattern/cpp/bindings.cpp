27b1cdc210a72fb86a1c91188597577401255847
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2024-02-28 13:04:38 -0700

    Update bindings.cpp

--- src/filepattern/cpp/bindings.cpp
+++ src/filepattern/cpp/bindings.cpp
@@ -37,23 +37,28 @@ PYBIND11_MODULE(backend, m){
         .def_static("inferPattern", py::overload_cast<const std::string&, std::string&, const std::string&>(&FilePattern::inferPattern))
         .def_static("inferPattern", py::overload_cast<std::vector<std::string>&, std::string&>(&FilePattern::inferPattern))
         .def("iterator", [](FilePattern &v){
+
             auto& pattern_obj = v.getPatternObject();
+
             if(pattern_obj != nullptr) {
+
                 if(pattern_obj->group_.size() == 0 || (pattern_obj->group_.size() != 0 && pattern_obj->group_[0] == "")){
 
-                        return py::make_iterator(pattern_obj->valid_files_.begin(), pattern_obj->valid_files_.end());
+                    return py::make_iterator(pattern_obj->valid_files_.begin(), pattern_obj->valid_files_.end());
                         
                 } else {
 
                     return py::make_iterator(pattern_obj->valid_grouped_files_.begin(), pattern_obj->valid_grouped_files_.end());
+                
                 }
             }
         }, py::keep_alive<0, 1>())
         .def("iteratorExternal", [](FilePattern &v){
+
             auto& pattern_obj = v.getPatternObject();
+
             if(pattern_obj != nullptr) {
                 
-
                 if(pattern_obj->group_.size() == 0 || (pattern_obj->group_.size() != 0 && pattern_obj->group_[0] == "")) {
 
                     v.next();
